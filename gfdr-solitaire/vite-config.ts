e0527740ed71816cbc719059a3607f9a8456a6f6
seek1r <2005cwj@gmail.com>
2026-07-24 18:12:01 -0400

    v0.1.1

--- gfdr-solitaire/vite-config.ts
+++ gfdr-solitaire/vite-config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // Replace '<repository-name>' with your actual GitHub repository name
-  base: '/<gfdr-solitaire>/',
+  base: '/gdfr-solitaire/',
 })
